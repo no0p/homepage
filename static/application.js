69d73f2311921dc5bcf2bba2e90dcb69ca68108d
robert berry <berrydigital@gmail.com>
2011-11-09 19:16:56 -0800

    cascade project images and carousel

---
+++ static/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   $("#waterwheel-carousel-default").waterwheelCarousel({
+      startingWaveSeparation: 0,
+      centerOffset: 30,
+      startingItemSeparation: 150,
+      itemSeparationFactor: .7,
+      itemDecreaseFactor: .75,
+      opacityDecreaseFactor: 1
+  });
+});
